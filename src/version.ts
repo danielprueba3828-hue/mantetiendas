ae92171e4adbd4410b26836634552ed972547c34
danielprueba3828-hue <danielprueba3828@gmail.com>
2026-08-27 05:02:37 -0500

    feat: add version manifest and metadata

---
+++ src/version.ts
@@ -0,0 +1,4 @@
+// ManteTiendas Version & Changelog
+export const APP_VERSION = "2.1.0-prod";
+export const LAST_UPDATE = "2026-08-27";
+export const DESIGNER = "Daniel Luna Software, Web & App Designer | © 2026";
