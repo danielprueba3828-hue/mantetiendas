e807881e94c4abe6163fb972ebf53054d405850e
danielprueba3828-hue <danielprueba3828@gmail.com>
2026-08-27 05:27:04 -0500

    feat: add data_stores_list.ts and data_users_list.ts

---
+++ src/data_stores_list.ts
@@ -0,0 +1,636 @@
+import type { Store } from './types';
+
+export const DEFAULT_STORES: Store[] = [
+  {
+    "id": 1,
+    "nombre": "BODEGA ATAHUALPA (BAT1)",
+    "ciudad": "Sierra",
+    "direccion": "BODEGA ATAHUALPA, Región Sierra",
+    "unidad": "BAT1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 2,
+    "nombre": "BODEGA NORTE (BNO1)",
+    "ciudad": "Sierra",
+    "direccion": "BODEGA NORTE, Región Sierra",
+    "unidad": "BNO1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 3,
+    "nombre": "BODEGA RIOBAMBA (BRI1)",
+    "ciudad": "Riobamba",
+    "direccion": "BODEGA RIOBAMBA, Región Sierra",
+    "unidad": "BRI1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 4,
+    "nombre": "CIKLA CUENCA (CCU1)",
+    "ciudad": "Cuenca",
+    "direccion": "CIKLA CUENCA, Región Sierra",
+    "unidad": "CCU1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 5,
+    "nombre": "EXPLORER JARDIN (EJA1)",
+    "ciudad": "Quito",
+    "direccion": "EXPLORER JARDIN, Región Sierra",
+    "unidad": "EJA1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 6,
+    "nombre": "EXPLORER MALL DEL RIO (ECU1)",
+    "ciudad": "Sierra",
+    "direccion": "EXPLORER MALL DEL RIO, Región Sierra",
+    "unidad": "ECU1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 7,
+    "nombre": "EXPLORER RIOBAMBA (ERI1)",
+    "ciudad": "Riobamba",
+    "direccion": "EXPLORER RIOBAMBA, Región Sierra",
+    "unidad": "ERI1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 8,
+    "nombre": "EXPLORER SCALA (ESC1)",
+    "ciudad": "Quito",
+    "direccion": "EXPLORER SCALA, Región Sierra",
+    "unidad": "ESC1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 9,
+    "nombre": "MARATHON JARDIN (MJA1)",
+    "ciudad": "Quito",
+    "direccion": "MARATHON JARDIN, Región Sierra",
+    "unidad": "MJA1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 10,
+    "nombre": "MARATHON MALL DEL ALTO (MMA1)",
+    "ciudad": "Sierra",
+    "direccion": "MARATHON MALL DEL ALTO, Región Sierra",
+    "unidad": "MMA1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 11,
+    "nombre": "MARATHON MALL DEL RIO (MCU1)",
+    "ciudad": "Sierra",
+    "direccion": "MARATHON MALL DEL RIO, Región Sierra",
+    "unidad": "MCU1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 12,
+    "nombre": "MARATHON QUICENTRO SUR (MQS1)",
+    "ciudad": "Quito",
+    "direccion": "MARATHON QUICENTRO SUR, Región Sierra",
+    "unidad": "MQS1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 13,
+    "nombre": "MARATHON RIOBAMBA (MRI1)",
+    "ciudad": "Riobamba",
+    "direccion": "MARATHON RIOBAMBA, Región Sierra",
+    "unidad": "MRI1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 14,
+    "nombre": "MARATHON SCALA (MSC1)",
+    "ciudad": "Quito",
+    "direccion": "MARATHON SCALA, Región Sierra",
+    "unidad": "MSC1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 15,
+    "nombre": "OUTLET BATAN (OBT1)",
+    "ciudad": "Sierra",
+    "direccion": "OUTLET BATAN, Región Sierra",
+    "unidad": "OBT1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 16,
+    "nombre": "OUTLET GUARANDA (OGU1)",
+    "ciudad": "Guaranda",
+    "direccion": "OUTLET GUARANDA, Región Sierra",
+    "unidad": "OGU1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 17,
+    "nombre": "OUTLET MONAY (OMO1)",
+    "ciudad": "Sierra",
+    "direccion": "OUTLET MONAY, Región Sierra",
+    "unidad": "OMO1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 18,
+    "nombre": "TAF JARDIN (FJA1)",
+    "ciudad": "Quito",
+    "direccion": "TAF JARDIN, Región Sierra",
+    "unidad": "FJA1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 19,
+    "nombre": "TAF QUICENTRO SUR (FQS1)",
+    "ciudad": "Quito",
+    "direccion": "TAF QUICENTRO SUR, Región Sierra",
+    "unidad": "FQS1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 20,
+    "nombre": "TELESHOP JARDIN (TJA1)",
+    "ciudad": "Quito",
+    "direccion": "TELESHOP JARDIN, Región Sierra",
+    "unidad": "TJA1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 21,
+    "nombre": "TELESHOP QUICENTRO SUR (TQS1)",
+    "ciudad": "Quito",
+    "direccion": "TELESHOP QUICENTRO SUR, Región Sierra",
+    "unidad": "TQS1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 22,
+    "nombre": "TELESHOP RIOBAMBA (TRI1)",
+    "ciudad": "Riobamba",
+    "direccion": "TELESHOP RIOBAMBA, Región Sierra",
+    "unidad": "TRI1",
+    "supervisorName": "JULIO TUBON"
+  },
+  {
+    "id": 23,
+    "nombre": "BODEGA SANGOLQUI (BSA1)",
+    "ciudad": "Sierra",
+    "direccion": "BODEGA SANGOLQUI, Región Sierra",
+    "unidad": "BSA1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 24,
+    "nombre": "BODEGA VALLE (BVA1)",
+    "ciudad": "Sierra",
+    "direccion": "BODEGA VALLE, Región Sierra",
+    "unidad": "BVA1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 25,
+    "nombre": "EXPLORER BOSQUE (EBO1)",
+    "ciudad": "Quito",
+    "direccion": "EXPLORER BOSQUE, Región Sierra",
+    "unidad": "EBO1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 26,
+    "nombre": "EXPLORER CONDADO (ECO1)",
+    "ciudad": "Quito",
+    "direccion": "EXPLORER CONDADO, Región Sierra",
+    "unidad": "ECO1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 27,
+    "nombre": "EXPLORER LATACUNGA (ELT1)",
+    "ciudad": "Latacunga",
+    "direccion": "EXPLORER LATACUNGA, Región Sierra",
+    "unidad": "ELT1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 28,
+    "nombre": "EXPLORER SAN LUIS (ESL1)",
+    "ciudad": "Quito",
+    "direccion": "EXPLORER SAN LUIS, Región Sierra",
+    "unidad": "ESL1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 29,
+    "nombre": "MARATHON BOSQUE (MBO1)",
+    "ciudad": "Quito",
+    "direccion": "MARATHON BOSQUE, Región Sierra",
+    "unidad": "MBO1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 30,
+    "nombre": "MARATHON CONDADO (MCO1)",
+    "ciudad": "Quito",
+    "direccion": "MARATHON CONDADO, Región Sierra",
+    "unidad": "MCO1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 31,
+    "nombre": "MARATHON LATACUNGA (MLT1)",
+    "ciudad": "Latacunga",
+    "direccion": "MARATHON LATACUNGA, Región Sierra",
+    "unidad": "MLT1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 32,
+    "nombre": "MARATHON SAN LUIS (MSL1)",
+    "ciudad": "Quito",
+    "direccion": "MARATHON SAN LUIS, Región Sierra",
+    "unidad": "MSL1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 33,
+    "nombre": "OUTLET DAPSILIA (ODP1)",
+    "ciudad": "Sierra",
+    "direccion": "OUTLET DAPSILIA, Región Sierra",
+    "unidad": "ODP1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 34,
+    "nombre": "OUTLET POMASQUI (OPO1)",
+    "ciudad": "Sierra",
+    "direccion": "OUTLET POMASQUI, Región Sierra",
+    "unidad": "OPO1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 35,
+    "nombre": "OUTLET VIVE PLAZA (OVP1)",
+    "ciudad": "Sierra",
+    "direccion": "OUTLET VIVE PLAZA, Región Sierra",
+    "unidad": "OVP1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 36,
+    "nombre": "PUMA CONDADO (PCO1)",
+    "ciudad": "Quito",
+    "direccion": "PUMA CONDADO, Región Sierra",
+    "unidad": "PCO1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 37,
+    "nombre": "TAF BOSQUE (FBO1)",
+    "ciudad": "Quito",
+    "direccion": "TAF BOSQUE, Región Sierra",
+    "unidad": "FBO1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 38,
+    "nombre": "TAF CONDADO (FCO1)",
+    "ciudad": "Quito",
+    "direccion": "TAF CONDADO, Región Sierra",
+    "unidad": "FCO1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 39,
+    "nombre": "TELESHOP LATACUNGA (TLT1)",
+    "ciudad": "Latacunga",
+    "direccion": "TELESHOP LATACUNGA, Región Sierra",
+    "unidad": "TLT1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 40,
+    "nombre": "TELESHOP SAN LUIS (TSL1)",
+    "ciudad": "Quito",
+    "direccion": "TELESHOP SAN LUIS, Región Sierra",
+    "unidad": "TSL1",
+    "supervisorName": "VICKY MONTIEL"
+  },
+  {
+    "id": 41,
+    "nombre": "BODEGA LOJA (BLJ1)",
+    "ciudad": "Loja",
+    "direccion": "BODEGA LOJA, Región Sierra",
+    "unidad": "BLJ1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 42,
+    "nombre": "BODEGA SAN AGUSTIN (BSG1)",
+    "ciudad": "Sierra",
+    "direccion": "BODEGA SAN AGUSTIN, Región Sierra",
+    "unidad": "BSG1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 43,
+    "nombre": "BODEGA VENTURA (BVE1)",
+    "ciudad": "Sierra",
+    "direccion": "BODEGA VENTURA, Región Sierra",
+    "unidad": "BVE1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 44,
+    "nombre": "CIKLA QUICENTRO NORTE (CQN1)",
+    "ciudad": "Quito",
+    "direccion": "CIKLA QUICENTRO NORTE, Región Sierra",
+    "unidad": "CQN1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 45,
+    "nombre": "EXPLORER LAGUNA MALL (ELG1)",
+    "ciudad": "Sierra",
+    "direccion": "EXPLORER LAGUNA MALL, Región Sierra",
+    "unidad": "ELG1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 46,
+    "nombre": "EXPLORER LOJA (ELJ1)",
+    "ciudad": "Loja",
+    "direccion": "EXPLORER LOJA, Región Sierra",
+    "unidad": "ELJ1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 47,
+    "nombre": "EXPLORER QUICENTRO NORTE (EQN1)",
+    "ciudad": "Quito",
+    "direccion": "EXPLORER QUICENTRO NORTE, Región Sierra",
+    "unidad": "EQN1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 48,
+    "nombre": "EXPLORER VENTURA (EVE1)",
+    "ciudad": "Sierra",
+    "direccion": "EXPLORER VENTURA, Región Sierra",
+    "unidad": "EVE1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 49,
+    "nombre": "MARATHON CCI (MCC1)",
+    "ciudad": "Quito",
+    "direccion": "MARATHON CCI, Región Sierra",
+    "unidad": "MCC1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 50,
+    "nombre": "MARATHON LA PLAZA (MIB1)",
+    "ciudad": "Sierra",
+    "direccion": "MARATHON LA PLAZA, Región Sierra",
+    "unidad": "MIB1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 51,
+    "nombre": "MARATHON LAGUNA (MLG1)",
+    "ciudad": "Sierra",
+    "direccion": "MARATHON LAGUNA, Región Sierra",
+    "unidad": "MLG1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 52,
+    "nombre": "MARATHON LOJA (MLJ1)",
+    "ciudad": "Loja",
+    "direccion": "MARATHON LOJA, Región Sierra",
+    "unidad": "MLJ1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 53,
+    "nombre": "MARATHON QUICENTRO NORTE (MQN1)",
+    "ciudad": "Quito",
+    "direccion": "MARATHON QUICENTRO NORTE, Región Sierra",
+    "unidad": "MQN1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 54,
+    "nombre": "OUTLET CAYAMBE (OCA1)",
+    "ciudad": "Cayambe",
+    "direccion": "OUTLET CAYAMBE, Región Sierra",
+    "unidad": "OCA1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 55,
+    "nombre": "OUTLET TULCAN (OTU1)",
+    "ciudad": "Tulcán",
+    "direccion": "OUTLET TULCAN, Región Sierra",
+    "unidad": "OTU1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 56,
+    "nombre": "PUMA QUICENTRO NORTE (PQN1)",
+    "ciudad": "Quito",
+    "direccion": "PUMA QUICENTRO NORTE, Región Sierra",
+    "unidad": "PQN1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 57,
+    "nombre": "TAF SCALA (FSC1)",
+    "ciudad": "Quito",
+    "direccion": "TAF SCALA, Región Sierra",
+    "unidad": "FSC1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 58,
+    "nombre": "UNDER ARMOUR QUICENTRO (UQN1)",
+    "ciudad": "Quito",
+    "direccion": "UNDER ARMOUR QUICENTRO, Región Sierra",
+    "unidad": "UQN1",
+    "supervisorName": "MARFA TORRES"
+  },
+  {
+    "id": 59,
+    "nombre": "BODEGA AMBATO (BAM1)",
+    "ciudad": "Ambato",
+    "direccion": "BODEGA AMBATO, Región Sierra",
+    "unidad": "BAM1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 60,
+    "nombre": "BODEGA RECREO (BRE1)",
+    "ciudad": "Quito",
+    "direccion": "BODEGA RECREO, Región Sierra",
+    "unidad": "BRE1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 61,
+    "nombre": "BODEGA SOLANDA (BSO1)",
+    "ciudad": "Quito",
+    "direccion": "BODEGA SOLANDA, Región Sierra",
+    "unidad": "BSO1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 62,
+    "nombre": "CIKLA AMBATO (CAM1)",
+    "ciudad": "Ambato",
+    "direccion": "CIKLA AMBATO, Región Sierra",
+    "unidad": "CAM1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 63,
+    "nombre": "CIKLA RIOCENTRO (CRC1)",
+    "ciudad": "Sierra",
+    "direccion": "CIKLA RIOCENTRO, Región Sierra",
+    "unidad": "CRC1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 64,
+    "nombre": "EXPLORER AMBATO (EAM1)",
+    "ciudad": "Ambato",
+    "direccion": "EXPLORER AMBATO, Región Sierra",
+    "unidad": "EAM1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 65,
+    "nombre": "EXPLORER PORTAL (ECP1)",
+    "ciudad": "Quito",
+    "direccion": "EXPLORER PORTAL, Región Sierra",
+    "unidad": "ECP1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 66,
+    "nombre": "EXPLORER RECREO (ERE1)",
+    "ciudad": "Quito",
+    "direccion": "EXPLORER RECREO, Región Sierra",
+    "unidad": "ERE1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 67,
+    "nombre": "EXPLORER RIOCENTRO (ERC1)",
+    "ciudad": "Sierra",
+    "direccion": "EXPLORER RIOCENTRO, Región Sierra",
+    "unidad": "ERC1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 68,
+    "nombre": "MARATHON AMBATO (MAM1)",
+    "ciudad": "Ambato",
+    "direccion": "MARATHON AMBATO, Región Sierra",
+    "unidad": "MAM1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 69,
+    "nombre": "MARATHON PASEO AMBATO (MAB1)",
+    "ciudad": "Ambato",
+    "direccion": "MARATHON PASEO AMBATO, Región Sierra",
+    "unidad": "MAB1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 70,
+    "nombre": "MARATHON PORTAL (MCP1)",
+    "ciudad": "Quito",
+    "direccion": "MARATHON PORTAL, Región Sierra",
+    "unidad": "MCP1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 71,
+    "nombre": "MARATHON RECREO (MRE1)",
+    "ciudad": "Quito",
+    "direccion": "MARATHON RECREO, Región Sierra",
+    "unidad": "MRE1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 72,
+    "nombre": "MARATHON SAN FRANCISCO (MSF1)",
+    "ciudad": "Sierra",
+    "direccion": "MARATHON SAN FRANCISCO, Región Sierra",
+    "unidad": "MSF1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 73,
+    "nombre": "OUTLET GASPAR (OGA1)",
+    "ciudad": "Sierra",
+    "direccion": "OUTLET GASPAR, Región Sierra",
+    "unidad": "OGA1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 74,
+    "nombre": "OUTLET GRANADOS (OGR1)",
+    "ciudad": "Sierra",
+    "direccion": "OUTLET GRANADOS, Región Sierra",
+    "unidad": "OGR1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 75,
+    "nombre": "PUMA PLAZA DE LAS AMERICAS (PAM1)",
+    "ciudad": "Sierra",
+    "direccion": "PUMA PLAZA DE LAS AMERICAS, Región Sierra",
+    "unidad": "PAM1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 76,
+    "nombre": "TAF PORTAL (FCP1)",
+    "ciudad": "Quito",
+    "direccion": "TAF PORTAL, Región Sierra",
+    "unidad": "FCP1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 77,
+    "nombre": "TAF RIOCENTRO (FRC1)",
+    "ciudad": "Sierra",
+    "direccion": "TAF RIOCENTRO, Región Sierra",
+    "unidad": "FRC1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 78,
+    "nombre": "TELESHOP AMBATO (TAM1)",
+    "ciudad": "Ambato",
+    "direccion": "TELESHOP AMBATO, Región Sierra",
+    "unidad": "TAM1",
+    "supervisorName": "LUIS VALLEJOS"
+  },
+  {
+    "id": 79,
+    "nombre": "TELESHOP RECREO (TRE1)",
+    "ciudad": "Quito",
+    "direccion": "TELESHOP RECREO, Región Sierra",
+    "unidad": "TRE1",
+    "supervisorName": "LUIS VALLEJOS"
+  }
+];
